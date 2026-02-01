f65f60eb724ba42aa0c4e91089a929771647a394
kofieduyankson-afk <kofi.edu.yankson@gmail.com>
2026-02-01 20:28:37 +0000

    structure fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
       {/* Hero */}
       <section className="py-20 text-center">
-        <h1>Layout Laurate LLC</h1>
+        <h1>Layout Laurate LLC  </h1>
         <h1 className="text-5xl font-bold mb-4 text-gray-900 dark:text-white">
           Building Digital Solutions
         </h1>
